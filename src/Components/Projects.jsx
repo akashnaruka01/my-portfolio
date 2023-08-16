bc01999a921705e8a699b8307ad52d311a0b130a
Akash Singh Naruka <akashnaruka01@gmail.com>
2023-08-16 20:15:18 +0530

    description

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -12,28 +12,28 @@ const Projects = () => {
   const boxProperty = {
     w: "25%",
     padding: "4",
-    bgColor: '#d8e9f9',
+    bgColor: "#d8e9f9",
     margin: "2rem",
     textAlign: "center",
     border: "2px solid black",
     borderRadius: "10%",
     height: "50vh",
-    transition: 'all 0.4s',
-    css:{
+    transition: "all 0.4s",
+    css: {
       "&:hover": {
         transform: "scale(1.02)",
-        boxShadow: '2px 2px black, -2px -2px black'
+        boxShadow: "2px 2px black, -2px -2px black",
       },
-    }
+    },
   };
 
-const imgPropety ={
-  objectFit: 'contain',
-  boxSizing: 'content-box',
-  w:'90%',
-  m:'8',
-  borderRadius:'5%',
-}
+  const imgPropety = {
+    objectFit: "contain",
+    boxSizing: "content-box",
+    w: "90%",
+    m: "8",
+    borderRadius: "5%",
+  };
 
   return (
     // <div id="proje">
@@ -48,7 +48,7 @@ const imgPropety ={
     //     ))}
     //   </div>
     // </div>
-    <Box id="projects" h={"150vh"} >
+    <Box id="projects" h={"150vh"}>
       <h1>Projects</h1>
       <Box
         className="pBox"
@@ -60,14 +60,12 @@ const imgPropety ={
         h={"120vh"}
       >
         <Box {...boxProperty} className="projectBox">
-          <Image
-            src={img1}
-            {...imgPropety}
-          />
+          <Image src={img1} {...imgPropety} />
           <Heading padding={10}>Usability Hub</Heading>
           <Text padding={10}>
-            It is a clone of platform that takes the guesswork out of design
-            decisions by validating them with real users.
+            Crafted a front-end clone of Usability Hub, revolutionizing design
+            validation by harnessing real user insights for optimized user
+            experiences.
           </Text>
           <Box
             p={15}
@@ -75,25 +73,29 @@ const imgPropety ={
             justifyContent={"space-evenly"}
             alignItems={"center"}
           >
-            <a className="anchor" href="https://github.com/akashnaruka01/Usabitlity-Hub">
+            <a
+              className="anchor"
+              href="https://github.com/akashnaruka01/Usabitlity-Hub"
+            >
               <AiFillGithub fontSize={"1.5rem"} color="black" />
               Source Code
             </a>
-            <a className="anchor" href="https://akashnaruka01.github.io/Usabitlity-Hub/">
+            <a
+              className="anchor"
+              href="https://akashnaruka01.github.io/Usabitlity-Hub/"
+            >
               <SiNetlify fontSize={"1.5rem"} />
               Go to site
             </a>
           </Box>
         </Box>
-      
+
         <Box {...boxProperty} className="projectBox">
-          <Image
-            src={img2}
-            {...imgPropety}
-          />
+          <Image src={img2} {...imgPropety} />
           <Heading padding={10}>My Portfolio</Heading>
           <Text padding={10}>
-           A simple web page using reactJs describing about myself in a very good way.
+            A captivating React-powered portfolio, elegantly showcasing my
+            creative journey and skills through immersive design.
           </Text>
           <Box
             p={15}
@@ -101,25 +103,26 @@ const imgPropety ={
             justifyContent={"space-evenly"}
             alignItems={"center"}
           >
-            <a className="anchor" href="">
+            <a
+              className="anchor"
+              href="https://github.com/akashnaruka01/my-portfolio/"
+            >
               <AiFillGithub fontSize={"1.5rem"} color="black" />
               Source Code
             </a>
-            <a className="anchor" href="">
+            <a className="anchor" href="/">
               <SiNetlify fontSize={"1.5rem"} />
               Go to site
             </a>
           </Box>
         </Box>
-      
+
         <Box {...boxProperty} className="projectBox">
-          <Image
-            src={img3}
-            {...imgPropety}
-          />
+          <Image src={img3} {...imgPropety} />
           <Heading padding={10}>Flick Player</Heading>
           <Text padding={10}>
-            A platform to play vedios and upload vedios, it has 10 vedio uploaded which can be played.
+            Immerse yourself in a curated collection of ten videos through a
+            dynamic React-based video player
           </Text>
           <Box
             p={15}
@@ -127,26 +130,30 @@ const imgPropety ={
             justifyContent={"space-evenly"}
             alignItems={"center"}
           >
-            <a className="anchor" href="https://github.com/akashnaruka01/Vedio-Player">
+            <a
+              className="anchor"
+              href="https://github.com/akashnaruka01/Vedio-Player"
+            >
               <AiFillGithub fontSize={"1.5rem"} color="black" />
               Source Code
             </a>
-            <a className="anchor" href="https://vedioplayer-akashnaruka01.netlify.app/">
+            <a
+              className="anchor"
+              href="https://vedioplayer-akashnaruka01.netlify.app/"
+            >
               <SiNetlify fontSize={"1.5rem"} />
               Go to site
             </a>
           </Box>
         </Box>
-      
+
         <Box {...boxProperty} className="projectBox">
-          <Image
-            src={img4}
-            {...imgPropety}
-          />
+          <Image src={img4} {...imgPropety} />
           <Heading padding={10}>Shopping Cart</Heading>
           <Text padding={10}>
-            It is a clone of platform that takes the guesswork out of design
-            decisions by validating them with real users.
+            Elevate your online shopping journey with a shopping cart project,
+            meticulously crafted using FastAPI and Redux Toolkit for streamlined
+            e-commerce management
           </Text>
           <Box
             p={15}
@@ -154,26 +161,30 @@ const imgPropety ={
             justifyContent={"space-evenly"}
             alignItems={"center"}
           >
-            <a className="anchor" href="https://github.com/akashnaruka01/redux-cart">
+            <a
+              className="anchor"
+              href="https://github.com/akashnaruka01/redux-cart"
+            >
               <AiFillGithub fontSize={"1.5rem"} color="black" />
               Source Code
             </a>
-            <a className="anchor" href="https://reduxcart-akashnaruka01.netlify.app/">
+            <a
+              className="anchor"
+              href="https://reduxcart-akashnaruka01.netlify.app/"
+            >
               <SiNetlify fontSize={"1.5rem"} />
               Go to site
             </a>
           </Box>
         </Box>
-      
+
         <Box {...boxProperty} className="projectBox">
-          <Image
-            src={img5}
-            {...imgPropety}
-          />
+          <Image src={img5} {...imgPropety} />
           <Heading padding={10}>Rookie Crypto</Heading>
           <Text padding={10}>
-            It is a clone of platform that takes the guesswork out of design
-            decisions by validating them with real users.
+            Embark on a real-time crypto exploration with my website, seamlessly
+            integrating Coingecko API to provide up-to-date cryptocurrency
+            insights
           </Text>
           <Box
             p={15}
@@ -181,7 +192,10 @@ const imgPropety ={
             justifyContent={"space-evenly"}
             alignItems={"center"}
           >
-            <a className="anchor" href="https://github.com/akashnaruka01/CryptoApp">
+            <a
+              className="anchor"
+              href="https://github.com/akashnaruka01/CryptoApp"
+            >
               <AiFillGithub fontSize={"1.5rem"} color="black" />
               Source Code
             </a>
@@ -191,11 +205,6 @@ const imgPropety ={
             </a>
           </Box>
         </Box>
-
-        
-        
-        
-        
       </Box>
     </Box>
   );
